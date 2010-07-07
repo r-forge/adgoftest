149a76a58b31afdb3b5fbec9d3fbda5f7c16bbda
cjgb (Mr ) <cgb@datanalytics.com>
2010-07-07 00:19:53 +0000

    Changed webpage.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adgoftest@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,51 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
-<!-- This is the project specific website template -->
-<!-- It can be changed as liked or replaced by other content -->
-
-<?php
-
-$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
-$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
-$themeroot='http://r-forge.r-project.org/themes/rforge/';
-
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
-
-  <head>
+<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
-  </head>
+	<title>R Package ADGofTest</title>
+	<link href="http://r-forge.r-project.org/themes/rforge/styles/estilo1.css" rel="stylesheet" type="text/css" />
+</head>
 
 <body>
 
 <!-- R-Forge Logo -->
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
 <tr><td>
-<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+<a href="/"><img src="http://r-forge.r-project.org/themes/rforge/imagesrf/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
 </table>
 
+<h2>Anderson-Darling Goodness of Fit Test R package</h2>
 
-<!-- get project title  -->
-<!-- own website starts here, the following may be changed as you like -->
+<p>This R package implements an approximate method to calculate the p-value for the Anderson-Darling test for uniformity based on J. and G. Marsaglia's 2004 paper <a href="http://www.jstatsoft.org/v09/i02/paper">&quot;Evaluating the Anderson-Darling Distribution&quot;</a>.</p>
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
+<p>You can find it in <a href="http://cran.r-project.org/web/packages/ADGofTest/index.html">CRAN</a> and can be installed using R's install.package function.</p>
 
-<!-- end of project description -->
+<p>The project summary page can be found <a href="http://r-forge.r-project.org/projects/adgoftest/">here</a>. You may also be interested in its page at <a href="http://crantastic.org/packages/ADGofTest">crantastic</a>.</p>
 
-<p> No content added. </p>
-
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
-
+<p>The package has been developed by <a href="http://www.datanalytics.com">Carlos J. Gil Bellosta</a>.</p>
 </body>
 </html>
+
